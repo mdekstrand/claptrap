8d246397cc33b6f7c2ee58cdf78fb0f48b04ad10
Michael Ekstrand <md@ekstrandom.net>
2016-02-14 16:19:52 -0600

    Write CSV file with Julian date

--- export.js
+++ export.js
@@ -2,13 +2,13 @@ const fs = require('fs'), os = require('os');
 const sqlite = require('sqlite3');
 
 var out = fs.createWriteStream('tweets.csv');
-out.write('ID\tUser\tTimestamp' + os.EOL);
+out.write('ID,User,Time' + os.EOL);
 
 var db = new sqlite.Database('tweets.db', function(err) {
   if (err) throw err;
-  db.each('SELECT tweet_id, user, timestamp, body FROM tweets', (err, row) => {
+  db.each("SELECT tweet_id, user, julianday(timestamp) - julianday('1900-01-01') AS date, body FROM tweets", (err, row) => {
     if (err) throw err;
-    out.write([row.tweet_id, row.user, row.timestamp].join('\t') + os.EOL);
+    out.write([row.tweet_id, row.user, row.date].join(',') + os.EOL);
   }, (err, n) => {
     if (err) throw err;
     console.log('wrote %d rows', n);
